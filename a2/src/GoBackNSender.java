52846af908b3c5966984ef4c5a1202ad48f08263
Qblack <blac2410@mylaurier.ca>
2015-02-28 22:13:25 -0500

    Weird timer issues

--- a2/src/GoBackNSender.java
+++ a2/src/GoBackNSender.java
@@ -1,15 +1,16 @@
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Timer;
+import java.util.TimerTask;
 
-/**
- * Created by blac2410 on 2/27/2015.
- */
 public class GoBackNSender {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         if(args.length!=6){
             System.out.format("Expected 6 arguments got: %d", args.length);
         }
@@ -22,17 +23,37 @@ public class GoBackNSender {
         int windowSize = Integer.parseInt(args[5]);
 
         Sender sender = new Sender(destinationAddress,destinationPort,senderPort,reliabilityNumber,windowSize);
-        sender.SendFile(fileName);
+        sender.start(fileName);
+
     }
 
     public static class Sender{
-        String destinationAddress;
-        int destinationPort;
-        int senderPort;
-        int reliabilityNumber;
-        int windowSize;
-        InetAddress m_receiver;
-        DatagramPacket EOT = new DatagramPacket("FIN".getBytes(),3,m_receiver,this.destinationPort);
+        private  static final int FILE_DATA_SIZE = 60;
+        private  static final int PACKET_SIZE = FILE_DATA_SIZE+1;
+        public static final int TIMEOUT = 4*100000000;
+        private String destinationAddress;
+        private int destinationPort;
+        private int senderPort;
+        private int reliabilityNumber;
+        private int windowSize;
+        private int nextSeqNum = 0;
+        private int sendBase=0;
+        private DatagramPacket[] sndpkt;
+        private InetAddress receiver;
+        private DatagramPacket eot;
+        private DatagramSocket socket;
+        private Timer timer = new Timer();
+        private final TimerTask start_timer = new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    timeout();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
 
         public Sender(String destinationAddress,int destinationPort,int senderPort, int reliabilityNumber, int windowSize) throws UnknownHostException {
             this.destinationAddress = destinationAddress;
@@ -40,28 +61,87 @@ public class GoBackNSender {
             this.senderPort = senderPort;
             this.reliabilityNumber = reliabilityNumber;
             this.windowSize = windowSize;
-            m_receiver = InetAddress.getByName(this.destinationAddress);
+            this.receiver = InetAddress.getByName(this.destinationAddress);
+            this.eot = new DatagramPacket("EOF".getBytes(),3, this.receiver,this.destinationPort);
         }
 
-        public void SendFile(String fileName) throws IOException {
-            DatagramSocket socket = new DatagramSocket(this.senderPort);
+        public void start(String fileName) throws IOException, InterruptedException {
+            socket = new DatagramSocket(this.senderPort);
 
             File fileHandle = new File(fileName);
             FileInputStream fileData = new FileInputStream(fileHandle);
 
-            byte[] buffer = new byte[124];
+            this.sndpkt = new DatagramPacket[(int) Math.ceil(fileHandle.length()/ FILE_DATA_SIZE)+1];
+
             int bytesRead = 0;
             int totalBytes = (int) fileHandle.length();
             int read = 0;
-            while (bytesRead<=totalBytes && read!=-1){
-                read = fileData.read(buffer, bytesRead, totalBytes-bytesRead);
+            byte[] data;
+            while (bytesRead<totalBytes && read!=-1) {
+                data = new byte[PACKET_SIZE];
+                int bytesToRead = Math.min(totalBytes - bytesRead, data.length - 1);
+                read = fileData.read(data, 1, bytesToRead);
+                bytesRead += read;
 
-                DatagramPacket packet = new DatagramPacket(buffer,buffer.length,m_receiver,this.destinationPort);
-                socket.send(packet);
+                boolean sent;
+                do{
+                    sent = rdt_send(data);
+                    byte[] tobugg = new byte[PACKET_SIZE];
+                    DatagramPacket receivedPacket = new DatagramPacket(tobugg,tobugg.length);
+                    socket.receive(receivedPacket);
+                    rdt_rcv(receivedPacket);
+                }while(!sent);
+
             }
-            socket.send(this.EOT);
 
+            socket.send(this.eot);
             socket.close();
+
         }
+
+        private boolean rdt_send(byte[] data) throws IOException, InterruptedException {
+            if(this.nextSeqNum<this.sendBase+this.windowSize){
+                this.sndpkt[this.nextSeqNum] = make_pkt(this.nextSeqNum, data);
+                this.socket.send(this.sndpkt[this.nextSeqNum]);
+                if(this.sendBase==this.nextSeqNum){
+                    timer.schedule(start_timer, TIMEOUT);
+                }
+                this.nextSeqNum= (this.nextSeqNum+1)%128;
+                return true;
+            }
+            return false;
+        }
+
+        private void timeout() throws IOException {
+            timer.schedule(start_timer,TIMEOUT);
+            for(int i=this.sendBase;i<this.nextSeqNum;i++){
+                this.socket.send(this.sndpkt[i]);
+            }
+        }
+
+        private void rdt_rcv(DatagramPacket rcvpkt){
+            this.sendBase = getacknum(rcvpkt)+1;
+            if(this.sendBase==this.nextSeqNum){
+                this.timer.cancel();
+            }else{
+//                this.timer.cancel();
+//                this.timer.purge();
+//                this.timer.schedule(this.start_timer,TIMEOUT);
+            }
+        }
+
+        private int getacknum(DatagramPacket rcvpkt) {
+            byte[] ack = rcvpkt.getData();
+            int sequenceNumber = Integer.parseInt(String.valueOf(ack[rcvpkt.getLength() - 1]));
+            return sequenceNumber % 128;
+        }
+
+
+        private DatagramPacket make_pkt(int nextSequenceNumber, byte[] data){
+            data[0] = (byte) nextSequenceNumber;
+            return new DatagramPacket(data,data.length, receiver, this.destinationPort);
+        }
+
+
     }
 }
